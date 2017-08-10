020bff2c54924140b146e484d8a7fd08e53df635
Joyfisher <joy.v.fisher@gmail.com>
2017-08-10 12:16:40 +0100

    linting problem on test fixed

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -6,17 +6,13 @@ const saveTestData = require('../seed/test.seed');
 const config = require('../config');
 var mongoose = require('mongoose');
 const db = config.DB[process.env.NODE_ENV] || process.env.DB;
-
+// console.log('******', db);
 
 describe('API', function () {
- let usefulIds;
+ // let usefulIds;
   before((done) => {
     mongoose.connection.dropDatabase()
-      .then(() => saveTestData(db, function (err, ids) {
-        if (err) throw err;
-        usefulIds = ids;
-        done();
-      }));
+      .then(() => saveTestData(db, done));
   });
   describe('GET /', function () {
     it('responds with status code 200', function (done) {
@@ -33,15 +29,15 @@ describe('API', function () {
   });
   describe('GET /topics', function () {
     it('responds with all topics', function (done) {
-
+     
       request(server)
         .get('/api/topics')
         .end((err, res) => {
           if (err) return console.log(err);
-          expect(res.status).to.equal(200);
-          expect(res.body.topics.length).to.equal(3);
-          done();
-
+            expect(res.status).to.equal(200);
+            expect(res.body.topics.length).to.equal(3);
+            done();
+  
         });
     });
   });
